6f722a95b6550190270e75347617504658c5db23
r0ballis0n <rob.allison@anobletech.com>
2019-01-30 13:39:37 -0500

    Editing

--- Add-Mailbox-Permissions-Groups-Menu.ps1
+++ Add-Mailbox-Permissions-Groups-Menu.ps1
@@ -1,23 +1,24 @@
 #Initial User Message
 Write-Host""
 Write-Host "============================================================"
-Write-Host "             Set Calendar Permisssion Script                "
-Write-Host "                Attorneys & Investigators                   "
+Write-Host "             Add Calendar Permisssion Script                "
+Write-Host "                Specified AD Group/Groups                   "
 Write-Host "============================================================"
 Write-Host ""
 Write-Host "This script will go through the setup process for giving a  "
-Write-Host "user access to another user's calendar. Please follow the    "
-Write-Host "prompts to complete the process. Don't forget to contact the"
-Write-Host "end user to verify that it worked as expected."
+Write-Host "user access to members of an ad secruity group's calendar.  "
+Write-Host "Please follow the prompts to complete the process. Don't "
+Write-Host "forget to contact the end user to verify that it worked as "
+Write-Host "expected."
 Write-Host ""
 
 #Variables
-$attorneymailboxes = @(get-ADGroupMember "Attorney Calendars" | ForEach-Object { Get-Mailbox $_.samAccountName })
-$investigatormailboxes = @(Get-ADGroupMember "Investigator Calendars") | ForEach-Object { Get-Mailbox $_.samAccountName })
+$adgroup1 = @(get-ADGroupMember "AD Group 1" | ForEach-Object { Get-Mailbox $_.samAccountName })
+$adgroup2 = @(Get-ADGroupMember "AD Group 2") | ForEach-Object { Get-Mailbox $_.samAccountName })
 
 #User Prompt for Name
-$User = Read-Host "Enter user's name to grant permissions to:"
+$User = Read-Host "Enter user's name to grant permissions to"
 
 #Commands
-foreach ($attorneymailbox in $attorneymailboxes) {Add-MailboxFolderPermission -Identity $attorneymailbox":\Calendar" -User $User -AccessRights Editor}
-foreach ($investigatormailbox in $investigatormailboxes ) {Add-MailboxFolderPermission -Identity $investigatormailbox":\Calendar" -User $User -AccessRights Editor}
+foreach ($adgroup1mailbox in $adgroup2mailboxes) {Add-MailboxFolderPermission -Identity $adgroup1mailbox":\Calendar" -User $User -AccessRights Editor}
+foreach ($adgroup2mailbox in $adgroup2mailboxes ) {Add-MailboxFolderPermission -Identity $adgroup2mailbox":\Calendar" -User $User -AccessRights Editor}
